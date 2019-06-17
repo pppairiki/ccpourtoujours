ccae8948b21ac250ec1d47f8b32cdc6a34513149
riki <liqiang.cui@iquantex.com>
2019-06-17 21:22:48 +0800

    feat: eureka-client register to eureka-server

--- eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/EurekaConsumerApplication.java
@@ -2,8 +2,15 @@ package com.ccpourtoujours.eurekaconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+/**
+ * @EnableEurekaClient 和 @EnableDiscoveryClient 区别
+ * 用的注册中心是eureka，那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient
+ * */
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
