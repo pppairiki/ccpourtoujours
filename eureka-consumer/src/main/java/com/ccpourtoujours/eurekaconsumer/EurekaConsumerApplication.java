ffbdd62125c7b736bc28a1d5279ea4e6cbd6710e
riki <liqiang.cui@iquantex.com>
2019-06-18 21:41:09 +0800

    feat: hystrix 服务降级

--- eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/EurekaConsumerApplication.java
@@ -2,20 +2,22 @@ package com.ccpourtoujours.eurekaconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
-@SpringBootApplication
+@EnableHystrix
+//  开启扫描spring cloud feign 客户端功能
+@EnableFeignClients
 @EnableEurekaClient
+@SpringBootApplication
 public class EurekaConsumerApplication {
 
     // 用来发起rest请求
-    @Bean
+/*    @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
--- eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/ccpourtoujours/eurekaconsumer/controller/ConsumerController.java
@@ -1,25 +1,18 @@
 package com.ccpourtoujours.eurekaconsumer.controller;
 
+import com.ccpourtoujours.eurekaconsumer.sao.HystricEurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    private LoadBalancerClient loadBalancerClient;
-    @Autowired
-    private RestTemplate restTemplate;
+    private HystricEurekaClient hystricEurekaClient;
 
     @GetMapping("/consumer")
     public String consumer() {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
-        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
-        System.out.println(url);
-        return restTemplate.getForObject(url, String.class);
+        return hystricEurekaClient.consumer();
     }
 }
